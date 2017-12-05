e6e1febf78c3f4d4014199b1775f8fb1ae601293
YI ZHUANG <urlikeahurricane@gmail.com>
2017-12-06 01:39:16 +0200

    adding nodemon and rows

--- server.js
+++ server.js
@@ -12,30 +12,30 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/category',function(req,res){
   knex.raw('select * from category').then(function(data){
-    res.send(data);
+    res.send(data.rows);
   })
 })
 app.get('/idea',function(req,res){
   knex.raw('select * from idea').then(function(data){
-    res.send(data);
+    res.send(data.rows);
   })
 })
 
 app.get('/member',function(req,res){
   knex.raw('select * from member').then(function(data){
-    res.send(data);
+    res.send(data.rows);
   })
 })
 
 app.get('/memberidea',function(req,res){
   knex.raw('select * from memb').then(function(data){
-    res.send(data);
+    res.send(data.rows);
   })
 })
 
 app.get('/comment',function(req,res){
   knex.raw('select * from comment').then(function(data){
-    res.send(data);
+    res.send(data.rows);
   })
 })
 
